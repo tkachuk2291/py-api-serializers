c12c042d81d4d9e0e17ec55ec82abaa581fbcf28
Anton Tkachuk <tkacuk2291@gmail.com>
2024-04-16 19:46:14 +0300

    solutions_x

--- cinema_service/urls.py
+++ cinema_service/urls.py
@@ -1,9 +1,7 @@
-import debug_toolbar
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/cinema/", include("cinema.urls", namespace="cinema")),
-    path("__debug__/", include("debug_toolbar.urls")),
 ]
